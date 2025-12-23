0944658e4094634334191368440aac931c8d4cc7
A-Wen <101863812+AWEN-0719@users.noreply.github.com>
2025-12-24 00:04:14 +0800

    Implement basic C program with printf examples
    Add basic C program demonstrating printf usage with various data types.
    
    Signed-off-by: A-Wen <101863812+AWEN-0719@users.noreply.github.com>

--- C語言基礎/20251221/main_1.c
+++ C語言基礎/20251221/main_1.c
@@ -1,1 +1,12 @@
+#include <stdio.h> // 引入標準輸入輸出函式庫
 
+int main(void) { // 主函式
+  printf("Hello World\n"); // \n 表示換行
+  printf("這是一個數值:%d\n",450); // %d 表示整數
+  printf("這是一個雙精準福點數:%.2lf\n",3.1415926); // %.2lf 表示只顯示小數點後兩位
+  printf("8 * 8 = %d\n",8*8); // int * int = int (運算元,整數型別int 8)(運算子 *)(運算元,整數型別int 8)
+  printf("5.5 * 3.3 = %lf\n",5.5*3.3); // 有小數點會從int自動轉換為 double * double = double
+  printf("5 / 3 = %lf\n", 5/3); // int / int = int
+  printf("5.0 / 3.0 = %.2lf\n", 5.0/3.0); // 有小數點會從int自動轉換為 double / double = double
+  return 0; // 回傳 0 表示程式正常結束
+}   
