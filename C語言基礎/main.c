6d1b28ef43513d8ab0667c92b467c9e6fe3ef7fc
A-Wen <101863812+AWEN-0719@users.noreply.github.com>
2025-12-23 23:26:14 +0800

    Enhance main.c with explanatory comments
    Added comments to explain the code functionality.
    
    Signed-off-by: A-Wen <101863812+AWEN-0719@users.noreply.github.com>

--- C語言基礎/main.c
+++ C語言基礎/main.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> // 引入標準輸入輸出函式庫
 
-int main(void) {
-  printf("Hello World\n");
-  printf("這是一個數值:%d\n",450);
-  printf("這是一個雙精準福點數:%.2lf\n",3.1415926);
-  printf("8 * 8 = %d",8*8);
-  return 0;
-}
+int main(void) { // 主函式
+  printf("Hello World\n"); // \n 表示換行
+  printf("這是一個數值:%d\n",450); // %d 表示整數
+  printf("這是一個雙精準福點數:%.2lf\n",3.1415926); // %.2lf 表示只顯示小數點後兩位
+  printf("8 * 8 = %d",8*8); // int * int = int
+  printf("5.5 * 3.3 = %lf",5.5*3.3); // double * double = double
+  return 0; // 回傳 0 表示程式正常結束
+} 
