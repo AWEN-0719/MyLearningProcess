342c3bbcd53bc6704a53db2bc2f69fe9ad18901b
A-Wen <101863812+AWEN-0719@users.noreply.github.com>
2025-12-24 01:04:54 +0800

    Update main_3.c
    Signed-off-by: A-Wen <101863812+AWEN-0719@users.noreply.github.com>

--- C語言基礎/20251221/main_3.c
+++ C語言基礎/20251221/main_3.c
@@ -7,9 +7,10 @@ int main(voide){
   double width = 5.8; // 宣告變數, 寬 width = 5.8
   double height = 9.2; // 宣告變數, 高 height = 9.2
   double area = width * height; // 宣告變數, 面積 area = width * height
-  printf("寬=%.1lf, 高=%.1lf , 面積是多少 ? \n ", width, height); // 印出字串與變數
+  printf("寬=%.1lf, 高=%.1lf , 面積是多少 ?\n ", width, height); // 印出字串與變數
+  printf("面積是 %.1lf * %.1lf = %.2lf \n", width, height, width*height); // 印出字串與變數
   printf("面積是 %.1lf * %.1lf = %.2lf \n", width, height, area); // 印出字串與變數
     
   return 0;
-// 回傳 0 給作業系統, 表示程式執行成功
+  // 回傳 0 給作業系統, 表示程式執行成功
 }
