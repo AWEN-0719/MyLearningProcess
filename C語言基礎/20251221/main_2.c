626b14ec8c03cf34cebcf04d760c85117e1811b3
A-Wen <101863812+AWEN-0719@users.noreply.github.com>
2025-12-24 00:02:28 +0800

    Correct syntax and enhance printf statements
    Fix syntax errors and improve output formatting.
    
    Signed-off-by: A-Wen <101863812+AWEN-0719@users.noreply.github.com>

--- C語言基礎/20251221/main_2.c
+++ C語言基礎/20251221/main_2.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main(voide){
+    printf("%d - %d = %d\n", 5, 3, 5-3); // 5 - 3 = 2
+    printf("%d + %d = %d\n", 5, 3, 5+3); // 5 + 3 = 8
+    printf("%d * %d = %d\n", 5, 3, 5*3); // 5 * 3 = 15
+    printf("%d / %d = %.2fl\n", 5, 3, 5/3.0); // 5 / 3 = 1.67
+    printf("%d / %d = %.2fl\n", 5, 3, (double)5/3); // 5 / 3 = 1.67 
+    printf("%d %% %d = %d\n", 5, 3, 5%3); // 5 % 3 = 2
+}
