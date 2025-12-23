c71ddee73fa3c745eeff06e5357e63b930f13e3d
A-Wen <101863812+AWEN-0719@users.noreply.github.com>
2025-12-23 23:05:09 +0800

    Add basic C program to print messages and values
    Signed-off-by: A-Wen <101863812+AWEN-0719@users.noreply.github.com>

---
+++ C語言基礎/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  printf("這是一個數值:%d\n",450);
+  printf("這是一個雙精準福點數:%.2lf\n",3.1415926);
+  printf("8 * 8 = %d",8*8);
+  return 0;
+}
